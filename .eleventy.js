9d7196809d6c11d16b096c597f6203ff6d332834
Georgios Vlassis <gvlassis@mailbox.org>
2023-12-12 13:25:29 +0100

    Merged all css

--- .eleventy.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(eleventyConfig) {
-    // Passthrough File Copy entries are relative to the root of your project and not your Eleventy input directory.
-    eleventyConfig.addPassthroughCopy("src/styles.css")
-    eleventyConfig.addPassthroughCopy("res/")
-    };
